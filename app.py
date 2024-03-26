0bcc62d197d4992992a0e801890137dca651f88b
qianq <qianq826@uw.edu>
2024-03-26 16:35:40 -0700

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title("😂Hi, my name is Sebastian")
+st.header("I am piece of 💩")
+
+st.image('./images/me.jpg')
+
+st.header("about me")
+st.markdown("""
+
+-school: UW
+-major: MSTI
+
+""")
+
+st.header("contact")
+st.markdown("""
+
+-email: qianq826@uw.edu
+-cellphone: 9735211660
+
+""")
+
